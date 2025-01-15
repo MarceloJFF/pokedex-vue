8fabc08fa47a6f211b13ec34ef9f295ebbc068d5
Marcelo Jhones <marceloj@acad.ifma.edu.br>
2025-01-15 20:30:49 -0300

    finalizacao das rotas e api

--- src/componentes/BarraDeNavagecao.vue
+++ src/componentes/BarraDeNavagecao.vue
@@ -14,7 +14,7 @@
         </li>
 
         <li class="nav-item">
-            <RouterLink class="nav-link" to="/geracao3">Segunda geração</RouterLink>
+            <RouterLink class="nav-link" to="/geracao3">Terceira geração</RouterLink>
         </li>
         
       </ul>
